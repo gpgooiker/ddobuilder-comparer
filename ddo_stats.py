c77e20c07a39d2c1bf4e1011116829d231720309
GP Gooiker <gp.gooiker@gmail.com>
2025-12-30 19:26:44 +0100

    Extract an integer

--- ddo_stats.py
+++ ddo_stats.py
@@ -1,2 +1,10 @@
 def melee_power(ddo_file: list) -> int:
-    return 32
+    label_string = 'Melee Power:  '
+    extracted_int = 0
+
+    for row in ddo_file:
+        if isinstance(row, str) and row.startswith(label_string):
+            stripped_string = row.strip(label_string)
+            extracted_int = int(float(stripped_string))
+
+    return extracted_int
