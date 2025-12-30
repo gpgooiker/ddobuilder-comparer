58a82bd67bfc1db2c17d5c30f51b13dbb6f40b6a
GP Gooiker <gp.gooiker@gmail.com>
2025-12-30 23:03:33 +0100

    Add more stats

--- ddo_stats.py
+++ ddo_stats.py
@@ -1,10 +1,20 @@
-def melee_power(ddo_file: list) -> int:
-    label_string = 'Melee Power:  '
+def get_stat(ddo_file: list, label_string: str) -> int:
+
     extracted_int = 0
 
     for row in ddo_file:
         if isinstance(row, str) and row.startswith(label_string):
-            stripped_string = row.strip(label_string)
-            extracted_int = int(float(stripped_string))
+            strip_label = row.strip(label_string)
+            strip_percentage = strip_label.strip("%")
+
+            extracted_int = int(float(strip_percentage))
 
     return extracted_int
+
+labels = {
+    "melee_power": "Melee Power:  ",
+    "doublestrike": "Doublestrike: ",
+    "mainhand_damage_ability_multiplier": "Mainhand damage ability multiplier: ",
+    "helpless_damage_bonus": "Helpless Damage bonus: "
+}
+
