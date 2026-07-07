78362541ffaf2b5e0890b69a84e7844cf6ccc5fc
GP <gp.gooiker@gmail.com>
2026-07-07 23:53:05 +0200

    Add get_stat function to ddo_file_parser for extracting stats from DDO build files

--- ddo_stats.py
+++ ddo_stats.py
@@ -1,4 +1,5 @@
 import logging
+from ddo_file_parser import get_stat
 logger = logging.getLogger(__name__)
 
 labels = {
@@ -13,50 +14,18 @@ labels = {
     "armor_class": "AC:"
 }
 
-def get_stat(ddo_file: list, label_string: str) -> float:
-    """The DDO builder by Maetrim exports a text file. Convert it to a list of strings, where every row in the text file is a new element in the list. Pass the
-    label of the stat you want as well, for instance, 'Melee Power:'. Be careful to copy these labels from the build export, there are invisible character
-    codes in the file.
-    
-    Example block from a build file:
-        Extract the defensive stats from this block in a ddo build file:
-                 Start Tome Final      HP:       6408      Displacement:   50%
-        Str:    10    8    42      Unc Rng:  -153      Incorp:          0%
-        Dex:     8    8    30      PRR:       384      AC:             185
-        Con:    18    8   103      MRR:       210      +Healing Amp:   146
-        Int:    10    8    24      Dodge:   16/27      -Healing Amp:    10
-        Wis:    10    8    30      Fort:     345%      Repair Amp:      10
-    """
-
-    extracted_float = float(0)
-
-    for row in ddo_file:
-        if isinstance(row, str) and label_string in row:
-            end_of_label_id = row.find(label_string) + len(label_string)
-            stripped_to_end_of_label = row[end_of_label_id:]
-            # Often, the ddo build files sport one or more spaces
-            stripped_up_to_stat = stripped_to_end_of_label.strip()
-
-            extracted_float = float(_get_stat_from_the_start_of_a_string(stripped_up_to_stat))
-
-            logger.debug(f'Extracted {label_string} {extracted_float}.')
-            return extracted_float
-
-    logger.debug(f'Extracted {label_string} {extracted_float}.')
-    return extracted_float
-
 def get_expected_damage(ddo_file: list) -> float:
     """In DDO, the player rolls a D20 to damage. Calculate the expected damage from one hit by adding all damage
     from 1 to 20 and then dividing this total by 20.
 
     We get the expected damage from one hit from this block in the build file:
         Main Hand: Ignition, the Fear and Flame
-        On Hit         5.65[1d10+3]+128
+        On Hit         5.65[1d10+3]+128
         Critical 17-18 (5.65[1d10+3]+139) * 2
         Critical 19-20 (5.65[1d10+3]+139) * 2"""
 
     average_damage_on_hit_calculated = False
-    on_hit_label = "On Hit         "
+    on_hit_label = "On Hit         "
     dice_multiplier = 0.0
     dice_average = 0.0
     average_damage_on_hit = 0.0
@@ -157,19 +126,6 @@ def normalize_armor_class(armor_class: float) -> float:
     max = 400.0
 
     return (armor_class - min) / (max - min)
-
-def _get_stat_from_the_start_of_a_string(string_starting_with_stat:str) -> int:
-    stat_as_array = []
-
-    for character in string_starting_with_stat:
-        if character.isdigit():
-            stat_as_array.append(character)
-        else:
-            # Stop if there's a % sign or a series of spaces
-            return int(''.join(stat_as_array))
-    
-    return int(''.join(stat_as_array))
-         
 
 def _is_a_dice_string(dice_string: str) -> bool:
     """A dice string has the format 2d8+2, wich means: 'roll two eight-sided dice and add 2'"""
